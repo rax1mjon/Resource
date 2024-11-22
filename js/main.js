dd719d3c5d17391580e8444892360a6effeefcb9
rax1mjon <tulaganovraximjon65@gmail.com>
2024-11-22 19:36:17 +0500

    animation end'

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
-// const loading = document.getElementById("loading");
+const loading = document.getElementById("loading");
 
-// const loadingDuration = 10000;
+const loadingDuration = 10000;
 
-// setTimeout(() => {
-//   loading.classList.add("loading-none");
-// }, loadingDuration);
+setTimeout(() => {
+  loading.classList.add("loading-none");
+}, loadingDuration);
 
-// window.addEventListener("scroll", function () {
-//   shrink();
-// });
+window.addEventListener("scroll", function () {
+  shrink();
+});
 
-// let navbar = document.getElementById("header");
+let navbar = document.getElementById("header");
 
-// function shrink() {
-//   if (scrollY > 780) {
-//     navbar.classList.add("navbar-shrink");
-//   } else {
-//     navbar.classList.remove("navbar-shrink");
-//   }
-// }
+function shrink() {
+  if (scrollY > 780) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+    navbar.classList.remove("navbar-shrink");
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const menuToggle = document.getElementById("humburger");
@@ -29,36 +29,19 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-// $(".owl-carousel").owlCarousel({
-//   loop: true,
-//   margin: 46,
-//   nav: true,
-//   responsive: {
-//     0: {
-//       items: 1,
-//       nav: false,
-//     },
-//     1035: {
-//       items: 2,
-//       nav: true,
-//       loop: false,
-//     },
-//   },
-// });
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
 
-// window.addEventListener("scroll", function () {
-//   toggleBacktop();
-// });
+let backtop = document.getElementById("backtop");
 
-// let backtop = document.getElementById("backtop");
-
-// function toggleBacktop() {
-//   if (
-//     document.body.scrollTop > 150 ||
-//     document.documentElement.scrollTop > 150
-//   ) {
-//     backtop.classList.add("backtop-show");
-//   } else {
-//     backtop.classList.remove("backtop-show");
-//   }
-// }
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 150 ||
+    document.documentElement.scrollTop > 150
+  ) {
+    backtop.classList.add("backtop-show");
+  } else {
+    backtop.classList.remove("backtop-show");
+  }
+}
